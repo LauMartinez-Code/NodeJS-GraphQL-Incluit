6a3d5927a7252faac563d83fe584f3a32c4899a9
Lautaro Martinez <laumartinez.exe@gmail.com>
2022-11-19 18:26:50 -0300

    Implementación de API de productos

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+const productsJSON = require('./products.json');
 const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    if (req.query.name) {
-        return res.status(200).json({
-            message: `Hello ${req.query.name}`
-        });
+    if (req.query.title) {
+        const title = req.query.title.toLocaleLowerCase();
+        const filteredProducts = productsJSON.products.filter(e => e.title.toLocaleLowerCase().includes(title));
+
+        if (filteredProducts.length > 0) {
+            return res.status(200).json(filteredProducts);
+        } else {
+            return res.status(404).json({ message : 'No products found.'});
+        }
     }
 
     next();
 });
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Hello world'
-    });
+app.get('/products', (req, res) => {
+    res.status(200).json(productsJSON);
 })
 
-app.listen(3000, () => console.log('API listening on http://localhost:3000'));
+app.listen(3000, () => console.log('API listening on http://localhost:3000/products'));
