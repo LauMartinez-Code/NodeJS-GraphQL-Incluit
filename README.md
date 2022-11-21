f92c59a15b2ff4b0367dc77f2a1ba065ebfa683d
Lautaro Martinez <laumartinez.exe@gmail.com>
2022-11-21 17:22:49 -0300

    Activity resolution Adds mongoose Dependency

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Clase 2
+# Clase 3
 
 ## Actividades
 - Crear una API rest para un listado de productos
+- Implementar MongoDB y Mongoose a la API rest de la actividad anterior
 
 ## To run this app:
 ### Download npm dependencies
@@ -10,3 +11,8 @@ At the project root folder run: `npm install`.
 ### Run
 To start Node app run: `node index.js` or `npm start`.  
 To test the queries, you can use Postman or any other software that allows HTTP requests.
+
+### Notes
+I added the script `init-mongo` becasue in the Linux distro that I use the `mongodb` service doesn't run automatically.
+Due to this, first time that I want to use the DB I have to start the service manually.  
+More info about this issue [here](https://github.com/Microsoft/WSL/issues/796#issuecomment-392995415)
--- index.js
+++ index.js
@@ -1,24 +1,50 @@
-const productsJSON = require('./products.json');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-app.use((req, res, next) => {
-    if (req.query.title) {
-        const title = req.query.title.toLocaleLowerCase();
-        const filteredProducts = productsJSON.products.filter(e => e.title.toLocaleLowerCase().includes(title));
+try {
+    mongoose.connect('mongodb://localhost:27017/products');
+    
+    mongoose.connection.once('open', () => console.log('DB connected'));
+    
+    const ProductSchema = new mongoose.Schema({
+        title: {
+            type: String,
+            required: true
+        },
+        price: Number
+    });
+    
+    const Product = mongoose.model('Product', ProductSchema);
+    
+    //Run this code once to fill the DB with some data. Then delete it or comment it again
+    // const productsJSON = require('./products.json');
+    // productsJSON.products.forEach(e => {
+    //     new Product(e).save()
+    //     .catch(error => console.error(`Something went wrong saving ${e}`, error));
+    // });
 
-        if (filteredProducts.length > 0) {
-            return res.status(200).json(filteredProducts);
-        } else {
-            return res.status(404).json({ message : 'No products found.'});
+    app.use(async (req, res, next) => {
+        if (req.query.title) {
+            const filteredProducts = await Product.find({ title: new RegExp(`${req.query.title}+`,"i")});
+
+            if (filteredProducts.length > 0) {
+                return res.status(200).json(filteredProducts);
+            } else {
+                return res.status(404).json({ message : 'No products found.' });
+            }
         }
-    }
+    
+        next();
+    });
+    
+    app.get('/products', async (req, res) => {
+        res.status(200).json(await Product.find());
+    });
+    
+    app.listen(3000, () => console.log('API listening on http://localhost:3000/products'));
 
-    next();
-});
+} catch (error) {
+    console.error(error);
+}
 
-app.get('/products', (req, res) => {
-    res.status(200).json(productsJSON);
-})
-
-app.listen(3000, () => console.log('API listening on http://localhost:3000/products'));
