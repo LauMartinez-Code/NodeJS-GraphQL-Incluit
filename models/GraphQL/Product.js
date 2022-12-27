db152eb0f9f81119d708736efd260d1f370fcb34
Lautaro Martinez <lautaro.martinez@incluit.com>
2022-12-26 23:45:41 -0300

    Fix resolvers parameters Add union example

--- models/GraphQL/Product.js
+++ models/GraphQL/Product.js
@@ -6,8 +6,21 @@ import { Product } from '../DB/Product.js';
 const typeDefs = loadSchemaSync('./models/GraphQL/Schema.graphql', { loaders: [new GraphQLFileLoader()] });
 
 const resolvers = {
+    Search: {
+        __resolveType(object) {
+            
+            if(object.email) {
+                return 'User';
+            }
+            else if(object.price) {
+                return 'Product';
+            }
+
+            return null;
+        }
+    },
     Query: {
-        getProductById: async (param) => {
+        getProductById: async (_, param) => {
             const filteredProduct = await Product.findById(param.id);
             
             if (filteredProduct)
@@ -16,19 +29,36 @@ const resolvers = {
                 throw new GraphQLError('HTTP 404. Product not found.', { extensions: { code: '404' } });
             
         },
-        getProductsByTitle: async (param) => {
+        getProductsByTitle: async (_, param) => {
             const filteredProducts = await Product.find({ title: new RegExp(`${param.title}+`,"i")});
             
             if (filteredProducts.length > 0)
                 return filteredProducts;
         },
         getAllproducts: async () => await Product.find(),
+        search: async (_, param) => {
+            //Do some searches using param.term...
+            //retruns diferent types of objects defined GQL Schema
+            return [
+                {
+                    "_id": "63a8eb118d14113ec9962f10",
+                    "title": "Samsung Universe 9",
+                    "price": 1249
+                },
+                {
+                    "_id": "a8eb118d14113ec932962f14",
+                    "name": "Jess",
+                    "email": "example@example.com",
+                    "role_id": 1
+                }
+            ];
+        }
     },
     
     Mutation: {
-        addProduct: async (param) => await Product.create(param.body),
+        addProduct: async (_, param) => await Product.create(param.body),
     
-        updateProductPrice: async (params) => {
+        updateProductPrice: async (_, params) => {
             const { id, price } = params;
     
             if (id && price) {
@@ -44,7 +74,7 @@ const resolvers = {
             }
     
         },
-        deleteProduct: async (param) => {
+        deleteProduct: async (_, param) => {
             const query = await Product.deleteOne({ _id: param?.id });
             console.log(`${query.deletedCount} product${query.deletedCount == 1 ? ' was' : 's were'} deleted`);
     
